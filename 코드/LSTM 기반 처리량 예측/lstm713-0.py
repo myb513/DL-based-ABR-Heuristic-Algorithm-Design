fd545d84735b102a563aadf92235cb760a199891
Iebin Moon <80698904+myb513@users.noreply.github.com>
2023-07-13 14:46:28 +0900

    (7.13)ver1.0 input data 변경 (throughput -> bandwidth)
    bandwidth 데이터로 변경 완료
    6800 여개 시점별 bandwidth 데이터를 학습하도록 수정.
    앞 30%를 test data로 설정.
    그러면, bus data가 많아지는 이슈 있지만,
    
    일치하는 시점을 파악하기 쉽도록 일단 그렇게 함

---
+++ 코드/LSTM 기반 처리량 예측/lstm713-0.py
@@ -0,0 +1,156 @@
+import numpy as np
+import os
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+import tensorflow as tf
+from keras.optimizers import Adam
+from tensorflow.keras.optimizers import RMSprop
+
+class LossCallback(tf.keras.callbacks.Callback):
+    def __init__(self):
+        self.total_loss = 0.0
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.total_loss += logs['loss']
+
+# 데이터를 로드하는 함수
+def load_data(file_path):
+    data = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip().split('\t')
+            time_stamp = float(line[0])
+            throughput = float(line[1])
+            data.append([time_stamp, throughput])
+    return np.array(data)
+
+# 데이터 전처리 함수
+def preprocess_data(data, timestep):
+    # 정규화
+    scaler = MinMaxScaler()
+    data_scaled = scaler.fit_transform(data)
+
+    # 입력 데이터와 타깃 데이터 분리
+    X, y = [], []
+    for i in range(len(data_scaled) - timestep):
+        X.append(data_scaled[i:i+timestep, 0])
+        y.append(data_scaled[i+timestep, 1])
+
+    X = np.array(X)
+    y = np.array(y)
+    return X, y, scaler
+
+# LSTM 모델 구성 함수
+def build_lstm_model(input_shape):
+    model = Sequential()
+    model.add(LSTM(64, input_shape=input_shape, return_sequences=True))
+    model.add(Dense(32))
+    model.add(LSTM(16, return_sequences=True))
+    model.add(Dense(8))
+    model.add(LSTM(8))
+    model.add(Dense(1))
+    return model
+
+# 데이터셋 경로
+# dataset_folder = 'dataset/'
+dataset_folder = 'dataset(bandwidth)/'
+# file_counts = {
+#     'norway_bus': 1,
+#     'norway_car': 1,
+#     'norway_ferry': 1,
+#     'norway_metro': 1,
+#     'norway_train': 1,
+#     'norway_tram': 1
+# }
+file_counts = {
+    'newdata': 1,
+}
+# 모델 학습 파라미터
+batch_size = 32
+epochs = 400
+timestep = 10  # timestep을 10로 설정
+test_ratio = 0.3  # 테스트 데이터 비율 (train: 0.7, test: 0.3)
+
+# 결과 저장 경로
+result_folder = 'results2/'
+os.makedirs(result_folder, exist_ok=True)
+
+# 각 파일에 대해 모델을 학습하고 예측하는 과정
+for file_prefix, count in file_counts.items():
+    total_loss = 0.0
+    total_rmse = 0.0  # 파일별 RMSE 누적 변수
+    for n in range(1, count + 1):
+        file_name = f"{file_prefix}_{n}"
+        file_path = os.path.join(dataset_folder, file_name)
+        data = load_data(file_path)
+        X, y, scaler = preprocess_data(data, timestep)
+
+        # # 데이터 분할
+        # test_size = int(len(X) * test_ratio)
+        # X_train, X_test = X[:-test_size], X[-test_size:]
+        # y_train, y_test = y[:-test_size], y[-test_size:]
+        # time_stamp_test = data[timestep:][len(data) - test_size:, 0]
+
+        # 데이터 분할 앞부분을 test data로 사종
+        test_size = int(len(X) * test_ratio)
+        X_train, X_test = X[:test_size], X[test_size:]
+        y_train, y_test = y[:test_size], y[test_size:]
+        time_stamp_test = data[:test_size, 0]
+
+        # LSTM 모델 구성
+        model = build_lstm_model(input_shape=(timestep, 1))  # 입력 차원을 (timestep, 1)로 변경
+
+        custom_adam = Adam(learning_rate=0.005,
+                           beta_1=0.9,  # 일차 모멘텀 추정값에 대한 지수 감쇠율, 일반적으로 0.9 권장
+                           beta_2=0.999,  # 이차 모멘텀 추정값에 대한 지수 감쇠율, 일반적으로 0.999 권장
+                           epsilon=1e-07,  # 0으로 나누는 것을 방지하기 위한 작은 상수
+                           amsgrad=False,  # AMSGrad를 사용할지 여부 (논문에서 제안된 Adam의 변형)
+                           decay=0.0
+                           )
+
+        custom_rmsprop = RMSprop(learning_rate=0.005,
+                                 rho=0.9,
+                                 momentum=0.0,
+                                 epsilon=1e-07,
+                                 centered=False,
+                                 name="RMSprop"
+                                 )
+
+        # 모델 컴파일
+        model.compile(optimizer=custom_rmsprop, loss='mean_squared_error')
+
+        # 모델 학습
+        loss_callback = LossCallback()  # LossCallback 객체 생성
+        model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, callbacks=[loss_callback])
+
+        # 예측
+        predicted_data_scaled = model.predict(X_test)
+        predicted_data = scaler.inverse_transform(np.concatenate((X_test[:, -1:], predicted_data_scaled), axis=1))
+        predicted_throughput = predicted_data[:, 1]
+
+        # 예측 결과 저장
+        result_file_name = f"{file_prefix}_{n}_result.txt"
+        result_file_path = os.path.join(result_folder, result_file_name)
+        with open(result_file_path, 'w') as result_file:
+            result_file.write("time_stamp\tActual Throughput\tPredicted Throughput\n")
+            for time, actual, predicted in zip(time_stamp_test, y_test, predicted_throughput):
+                result_file.write(f"{time}\t{actual}\t{predicted}\n")
+
+        # 전체 손실 및 RMSE 누적
+        total_loss += loss_callback.total_loss
+        rmse = np.sqrt(mean_squared_error(y_test, predicted_throughput))
+        total_rmse += rmse
+
+        print(f"File: {file_name}")
+        print(f"Result saved: {result_file_path}")
+        print(f"RMSE for {file_name}: {rmse}")
+        print('-----------------------------')
+
+    # 파일별 평균 손실 및 RMSE 출력
+    average_loss = total_loss / count
+    average_rmse = total_rmse / count
+    print(f"Average Loss for {file_prefix}: {average_loss}")
+    print(f"Average RMSE for {file_prefix}: {average_rmse}")
+    print('=============================')
